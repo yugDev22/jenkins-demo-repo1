67268b792e12304a4bb68ae3e0024612eb7a17f3
Yugam Bhattania <57980950+yugDev22@users.noreply.github.com>
2022-09-14 12:18:22 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main{
   public static void main(String args[]){
     System.out.println("Hello there, welcome to Jenkins tutorial!!");
+    System.out.println("added new line");
     System.out.println("byee...");
   }
 }
